f5636af207964bb88324e4f4652b72e342ecb69c
Rémy <remyc59440@hotmail.com>
2022-11-30 15:18:20 +0100

    exo 79 ok

--- exo.js
+++ exo.js
@@ -1,3 +1,22 @@
 let pomme = "Elle est belle ma pomme !";
+let indexPomme = document.getElementById("index-pomme");
+let lastIndex = document.getElementById("last-index-m");
+let myPomme = document.getElementById("pomme");
+let myOne = document.getElementById("one-line");
 
-console.log(pomme.lastIndexOf('m'));
+indexPomme.innerHTML = pomme;
+lastIndex.innerHTML = pomme;
+
+console.log(pomme.lastIndexOf("m"));
+console.log(pomme.indexOf("pomme"))
+
+myPomme.innerHTML = pomme.substring(17, 23)
+
+if (pomme.includes("Elle")){
+    console.log(pomme + "Commence par elle")
+}
+if (pomme.lastIndexOf("!")){
+    console.log(pomme + "Se termine par un point d'exclamation")
+}
+
+myOne.innerHTML = pomme.slice()
